f6c3ecee264b716924af88f3e4b326b0094d0a7a
amit@SUI <amit.chanchal@sustainurban.org>
2016-06-01 17:43:41 +0530

    added base files, extended scaffold!

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
+var exhbs = require('express-handlebars');
 
 var app = express();
+
+app.engine('handlebars', exhbs({defaultLayout:'main'}));
+app.set('view engine', 'handlebars');
 
 app.get('/', function(req,res){
 	res.send("hello");
